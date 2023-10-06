c93ec7bec2af3197412e78c50880f5742caeda8a
JuanPabloSaavedraRamirez <alphaoyster714@gmail.com>
2023-10-05 21:19:49 -0600

    Finalizado
    Ya solo falta subirlo, ya es lo mas facil

--- src/MainApp.tsx
+++ src/MainApp.tsx
@@ -26,6 +26,14 @@ const MainApp = () => {
         como desarrollador, es para la materia de proyecto semestral</h3>
       </div>
 
+      <div className='Proyecto3'>
+        <img className='ImgProyecto3' src="plotter-con-Raspberry-Pi.jpg" alt="" />
+        <h3 className='TextoProyecto3'>En el segundo semestre desarrolle junto a
+        una compañera un plotter completamente funcional, con servomotores, una Raspberry
+        , palos de madera y un lapiz, atravez de cordenadas podias hacer dibujos
+        lo siguiente es una imagen de referencia</h3>
+      </div>
+      <br /><br /><br /><br /><br />
       <div className="conocimientos">
         <h2 className="Texto">Conocimiento en legunajes de programacion</h2>
         <h3 className="Malo">Malo</h3>
@@ -52,20 +60,24 @@ const MainApp = () => {
           junto con esto se manejar el html, css y en parte JavaScrip y TypeScrip, aunque estos ultimos aun
           se me dificultan algo, acontinuacion veran unas animaciones en css que se realizar
         </h3>
+        <h3>Aqui un cuadrito el cual va a girar en un bucle infinito</h3>
         <div className="Container">
           <div className="Cuadrito"></div>
         </div>
         <br /><br />
+        <h3>Al pasar el raton sobre el contenedor este se va a expandir con una
+          animacion la cual hara que por segundos se haga mas grande
+        </h3>
         <div className="Container">
           <div className="expansion"></div>
         </div>
         <br /><br />
+        <h3>Al pasar el raton por el texto este cambiara su estilo y color</h3>
         <div className="Container">
           <div className="Hoverme">Hover me</div>
         </div>
         <br /><br />
       </div>
-
 
       <div className="Final">
         <h3 className="linkedlnT">Linkdln: (personal)</h3><a className="Linkdln" href="https://www.linkedin.com/in/juan-pablo-saavedra-ram%C3%ADrez-77ab15288"><img className="LinI" src="in.png" alt="" /></a>
